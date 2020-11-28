ff6e97683173a4eafef14e55462eb6d9b51adfcd
fuhr3r <gabrielferreira019@gmail.com>
2020-11-28 01:22:24 -0300

    Last update before production

--- js/main.js
+++ js/main.js
@@ -1,38 +1,69 @@
 let times = 0;
+let hasBalloon = false;
 
 function changeAntero() {
   const face = document.getElementById('antero');
+  const link = document.getElementById('link');
+  const balloon = document.getElementById('balloon');
   const audio = document.getElementById('audio');
   const audio1 = document.getElementById('audio1');
   const audio2 = document.getElementById('audio2');
 
+  if (hasBalloon) {
+    balloon.style.left = '100%';
+    setTimeout(() => balloon.style.left = '78%', 400)
+  } else {
+    balloon.style.left = '78%';
+  }
+
   const array = [
     { audio: audio, timeout: 600 },
-    { audio: audio1, timeout: 1800 },
-    { audio: audio2, timeout: 2500 }
+    { audio: audio1, timeout: 1650 },
+    { audio: audio2, timeout: 2350 }
   ];
 
   const item = array[Math.floor(Math.random() * array.length)];
 
-  // const times = Math.floor(Math.random() * 10);
   times++;
   const random = Math.floor(Math.random() * 5);
-  console.log({times, random});
 
+  hasBalloon = true;
   if (times === 8) {
+    link.classList.toggle('disabled');
+
     times = 0;
-    face.style.backgroundImage="url(img/antero_on.png)";
+    face.style.backgroundImage="url(img/fechada.png)";
 
     if (random === 1) {
       item.audio.play();
-      return setTimeout(() =>
-        face.style.backgroundImage="url(img/antero_off.png)",
+      face.style.backgroundImage="url(img/aberta.png)";
+
+      setTimeout(() => {
+          face.style.backgroundImage="url(img/fechada.png)"
+          setTimeout(() => face.style.backgroundImage="url(img/antero_off.png)", 150)
+        },
         item.timeout
       );
+
+      return setTimeout(() => {
+        balloon.style.left = '100%';
+        link.classList.toggle('disabled');
+      }, 1000);
     }
 
     audio.play();
+    face.style.backgroundImage="url(img/aberta.png)";
 
-    setTimeout(() => face.style.backgroundImage="url(img/antero_off.png)", 600);
+    setTimeout(() => {
+      face.style.backgroundImage="url(img/fechada.png)"
+      setTimeout(() => face.style.backgroundImage="url(img/antero_off.png)", 150)
+    }, 500);
+
+    setTimeout(() => {
+      balloon.style.left = '100%';
+      link.classList.toggle('disabled');
+    }, 1300);
+
+    // link.classList.toggle('disabled');
   }
 }
